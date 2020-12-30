4fe857befd69e988586dc6de765a23196624af90
ValentinCerneanu <valentincerneanu28@gmail.com>
2020-12-31 01:23:04 +0200

    add Jung library

--- src/main/java/com/mycompany/phase2/controllers/UploadRDFController.java
+++ src/main/java/com/mycompany/phase2/controllers/UploadRDFController.java
@@ -18,6 +18,22 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+
+
+import edu.uci.ics.jung.algorithms.layout.*;
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.graph.DirectedGraph;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.util.EdgeType;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+
+import javax.swing.*;
+import java.awt.*;
 
 /**
  *
@@ -38,10 +54,61 @@ public class UploadRDFController extends HttpServlet{
         }
 
         model.read(fileContent, null);
-        model.write(System.out);
+        //model.write(System.out);
+        
+        buildGraph(model);
+        
+        StmtIterator iter = model.listStatements();
+        while (iter.hasNext()) {
+            Statement stmt      = iter.nextStatement();  // get next statement
+            Resource  subject   = stmt.getSubject();     // get the subject
+            Property  predicate = stmt.getPredicate();   // get the predicate
+            RDFNode   object    = stmt.getObject();      // get the object
+
+            System.out.print(subject.toString());
+            System.out.print(" " + predicate.toString() + " ");
+            if (object instanceof Resource) {
+               System.out.print(object.toString());
+            } else {
+                // object is a literal
+                System.out.print(" \"" + object.toString() + "\"");
+            }
+
+            System.out.println(" .");
+        }
         
         processRequest(request, response);
     }
+    
+    private void buildGraph(Model model){
+        DirectedGraph<RDFNode,Statement> g = new DirectedSparseGraph<>();
+        boolean includeLiterals = true;
+        
+        StmtIterator iter = model.listStatements();
+        while(iter.hasNext()){
+            Statement stm = iter.nextStatement();
+            RDFNode sub = stm.getSubject();
+            RDFNode obj = stm.getObject();
+            g.addVertex(sub);
+            if(includeLiterals || !obj.isLiteral()){
+                g.addVertex(obj);
+                g.addEdge(stm, sub, obj, EdgeType.DIRECTED);
+            }
+        }
+        
+        Layout<Integer, String> layout = new FRLayout(g);
+        layout.setSize(new Dimension(800, 800)); // sets the initial size of the space
+        // The BasicVisualizationServer<V,E> is parameterized by the edge types
+        BasicVisualizationServer<Integer,String> vv = new BasicVisualizationServer<>(layout);
+        vv.setPreferredSize(new Dimension(450,450)); //Sets the viewing area size
+
+        JFrame frame = new JFrame("Simple Graph View");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(vv);
+        frame.pack();
+        frame.setVisible(true); 
+    }
+    
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
